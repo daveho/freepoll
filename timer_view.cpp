5c18f5e098d3a03bdc6e70f96eca497e5cf0a237
David Hovemeyer <david.hovemeyer@gmail.com>
2022-08-06 16:26:57 -0400

    added license info

--- timer_view.cpp
+++ timer_view.cpp
@@ -1,3 +1,20 @@
+// Copyright (c) 2022, David Hovemeyer <david.hovemeyer@gmail.com>
+
+// This file is part of FreePoll.
+//
+// FreePoll is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// FreePoll is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with FreePoll. If not, see <https://www.gnu.org/licenses/>.
+
 #include <iostream>
 #include <sstream>
 #include <iomanip>
@@ -24,7 +41,7 @@ TimerView::~TimerView() {
 }
 
 void TimerView::on_update(Observable *observable, int hint) {
-  std::cout << "timer sent a notification\n";
+  //std::cout << "timer sent a notification\n";
 
   // this is called from the timer thread, so post an event
   // which will ensure that the GUI update happens in the main
@@ -34,7 +51,7 @@ void TimerView::on_update(Observable *observable, int hint) {
 }
 
 void TimerView::on_timer_update(wxCommandEvent &evt) {
-  std::cout << "processing timer event in view\n";
+  //std::cout << "processing timer event in view\n";
 
   if (!m_timer->is_running()) {
     m_label->SetLabel("");
