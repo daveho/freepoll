d08c7fbd1c5fa1b8733fb94094aa11a1932c11a5
David Hovemeyer <david.hovemeyer@gmail.com>
2022-08-06 14:15:33 -0400

    added a button

--- poll_view.cpp
+++ poll_view.cpp
@@ -1,8 +1,36 @@
+// Copyright (c) 2022, David Hovemeyer <david.hovemeyer@gmail.com>
+
+// This file is part of FreePoll.
+//
+// FreePoll is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// FreePoll is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with FreePoll. If not, see <https://www.gnu.org/licenses/>.
+
+#include <iostream>
+#include <wx/button.h>
 #include "poll_view.h"
+//#include "play_button_icon.h"
+
+const int PLAY_STOP_BUTTON = 100;
 
 PollView::PollView(wxWindow *parent)
   : wxPanel(parent, wxID_ANY, wxDefaultPosition, wxSize(320, 96))
   , m_poll(nullptr) {
+  wxBoxSizer *items = new wxBoxSizer(wxHORIZONTAL);
+
+  wxButton *poll_control_btn = new wxButton(this, PLAY_STOP_BUTTON, "", wxDefaultPosition, wxSize(40, 40));
+  items->Add(poll_control_btn);
+
+  SetSizer(items);
 }
 
 PollView::~PollView() {
@@ -11,3 +39,11 @@ PollView::~PollView() {
 void PollView::on_update(Observable *observable, int hint) {
   // TODO
 }
+
+void PollView::on_play_stop_button(wxCommandEvent &evt) {
+  std::cout << "button pressed!\n";
+}
+
+wxBEGIN_EVENT_TABLE(PollView, wxPanel)
+  EVT_BUTTON(PLAY_STOP_BUTTON, PollView::on_play_stop_button)
+wxEND_EVENT_TABLE()
--- poll_view.h
+++ poll_view.h
@@ -1,7 +1,25 @@
+// Copyright (c) 2022, David Hovemeyer <david.hovemeyer@gmail.com>
+
+// This file is part of FreePoll.
+//
+// FreePoll is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// FreePoll is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with FreePoll. If not, see <https://www.gnu.org/licenses/>.
+
 #ifndef POLL_VIEW_H
 #define POLL_VIEW_H
 
 #include <wx/panel.h>
+#include <wx/sizer.h>
 #include "observer.h"
 
 class Poll;
@@ -15,6 +33,11 @@ public:
   virtual ~PollView();
 
   virtual void on_update(Observable *observable, int hint);
+
+private:
+  void on_play_stop_button(wxCommandEvent &evt);
+
+  wxDECLARE_EVENT_TABLE();
 };
 
 #endif // POLL_VIEW_H
