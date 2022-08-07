c7debb8f95ae04e5500a44358f398495e7e12e91
David Hovemeyer <david.hovemeyer@gmail.com>
2022-08-07 09:40:53 -0400

    code cleanup

--- main.cpp
+++ main.cpp
@@ -26,58 +26,6 @@
 #include "base.h"
 #include "poll_runner.h"
 
-#if 0
-// this is just for testing
-class PrintResponses : public PollResponseCollector {
-private:
-  PrintResponses(const PrintResponses &);
-  PrintResponses &operator=(const PrintResponses &);
-
-public:
-  PrintResponses(Poll &poll);
-  virtual ~PrintResponses();
-
-  virtual void on_response(RemoteID remote_id, Option option);
-};
-
-PrintResponses::PrintResponses(Poll &poll)
-  : PollResponseCollector(poll) {
-}
-
-PrintResponses::~PrintResponses() {
-}
-
-void PrintResponses::on_response(RemoteID remote_id, Option option) {
-  PollResponseCollector::on_response(remote_id, option);
-
-  std::stringstream ss;
-  ss << std::hex;
-  ss << "RemoteID:" << remote_id << ", option=" << char('A' + int(option));
-  std::cout << ss.str() << "\n";
-}
-
-namespace {
-
-void watch_poll(Base *base, ResponseCallback *response_callback, volatile const bool *stop) {
-  base->start_poll(Base::ALPHA);
-  base->collect_responses(*stop, response_callback);
-  base->stop_poll();
-}
-
-std::string option_to_str(Option option) {
-  switch (option) {
-  case Option::A: return "A";
-  case Option::B: return "B";
-  case Option::C: return "C";
-  case Option::D: return "D";
-  case Option::E: return "E";
-  default:        return "?";
-  }
-}
-
-}
-#endif
-
 int main() {
   std::cout << "Initalizing base...\n";
   std::unique_ptr<Base> base(new Base());
@@ -90,9 +38,6 @@ int main() {
             << "the program\n";
 
   Poll poll;
-  //PrintResponses print_responses(poll);
-  //std::thread *poll_watcher = nullptr;
-  //bool stop = false; // will control when poll stops
   PollRunner runner(base.get(), &poll);
 
   bool done = false;
@@ -100,13 +45,9 @@ int main() {
   while (!done && std::getline(std::cin, line)) {
     if (line == "start" && !runner.is_poll_started()) {
       std::cout << "starting poll...\n";
-      //poll_watcher = new std::thread( watch_poll, base.get(), &print_responses, &stop);
       runner.start_poll();
     } else if (line == "stop") {
       if (runner.is_poll_started()) {
-        //stop = true;
-        //poll_watcher->join();
-        //delete poll_watcher;
         runner.stop_poll();
         std::cout << "poll finished\n";
       }
