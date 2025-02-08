aa40d9ec6a1e8a2ee294dfb4567d517c47ab12a6
David Hovemeyer <david.hovemeyer@gmail.com>
2025-02-08 14:26:08 -0500

    display frquency based on selected course

--- include/F_freepoll_window.h
+++ include/F_freepoll_window.h
@@ -26,27 +26,37 @@ public:
   static const int WIDTH = 360;
 
   static const int COURSE_CHOOSER_Y = 0;
-  static const int COURSE_CHOOSER_HEIGHT = 32;
+  static const int COURSE_CHOOSER_HEIGHT = 24;
   static const int CONTROLS_Y = COURSE_CHOOSER_Y + COURSE_CHOOSER_HEIGHT;
-  static const int CONTROLS_HEIGHT = 64;
+  static const int CONTROLS_HEIGHT = 54;
   static const int BARGRAPH_Y = CONTROLS_Y + CONTROLS_HEIGHT;
-  static const int BARGRAPH_HEIGHT = 200;
+  static const int BARGRAPH_HEIGHT = 140;
 
-  static const int HEIGHT = COURSE_CHOOSER_HEIGHT + CONTROLS_HEIGHT + BARGRAPH_HEIGHT;
-  static const int HEIGHT_NOGRAPH = COURSE_CHOOSER_HEIGHT + CONTROLS_HEIGHT;
+  static const int HEIGHT = COURSE_CHOOSER_HEIGHT + CONTROLS_HEIGHT + BARGRAPH_HEIGHT + SPACING*2;
+  static const int HEIGHT_NOGRAPH = COURSE_CHOOSER_HEIGHT + CONTROLS_HEIGHT + SPACING;
 
+  static const int CONTROL_Y = SPACING;
+
+  static const int POLL_BTN_X = 0;
   static const int POLL_BTN_WIDTH = CONTROLS_HEIGHT - SPACING*2;
   static const int POLL_BTN_HEIGHT = POLL_BTN_WIDTH;
 
-  static const int TIMER_DISPLAY_WIDTH = 100;
+  static const int TIMER_DISPLAY_X = POLL_BTN_X + POLL_BTN_WIDTH + SPACING;
+  static const int TIMER_DISPLAY_WIDTH = 96;
   static const int TIMER_DISPLAY_HEIGHT = POLL_BTN_HEIGHT;
 
+  static const int COUNT_DISPLAY_X = TIMER_DISPLAY_X + TIMER_DISPLAY_WIDTH + SPACING;
   static const int COUNT_DISPLAY_WIDTH = 80;
   static const int COUNT_DISPLAY_HEIGHT = POLL_BTN_HEIGHT;
 
-  static const int GAP_WIDTH = 50;
+  static const int FREQ_DISPLAY_X = COUNT_DISPLAY_X + COUNT_DISPLAY_WIDTH + SPACING;
+  static const int FREQ_DISPLAY_WIDTH = 64;
+  static const int FREQ_DISPLAY_HEIGHT = POLL_BTN_HEIGHT;
+
+  static const int TEXT_SIZE = POLL_BTN_HEIGHT - 10;
 
   static const int GRAPH_BTN_WIDTH = POLL_BTN_WIDTH;
+  static const int GRAPH_BTN_X = WIDTH - GRAPH_BTN_WIDTH;
   static const int GRAPH_BTN_HEIGHT = POLL_BTN_WIDTH;
 
   static const Fl_Color DISABLED_TEXT_COLOR = 0x80808000;
@@ -58,17 +68,21 @@ private:
   bool m_graph_shown;
 
   // containers
-  Fl_Tile m_tile;
+  Fl_Pack m_pack; // overall container for window contents
+
   Fl_Choice m_course_chooser;
-  Fl_Pack m_controls;
+  
+  Fl_Group m_controls;
   Fl_Button m_poll_btn;
   Fl_Box m_timer_display;
   Fl_Box m_count_display;
-  Fl_Box m_gap;
+  Fl_Box m_freq_display;
   Fl_Button m_graph_btn;
   Fl_End m_controls_end;
+
   Fl_Box m_graph_box;
-  Fl_End m_tile_end;
+
+  Fl_End m_pack_end;
 
 public:
   F_FreePollWindow( Base *base, DataStore *datastore );
@@ -77,9 +91,11 @@ public:
   void show( int argc, char **argv );
   virtual void on_update(Observable *observable, int hint);
 
+  static void on_course_change( Fl_Widget *w, void *data );
   static void on_graph_button_clicked( Fl_Widget *w, void *data );
 
 private:
+  void update_frequency_display();
 };
 
 #endif // F_FREEPOLL_WINDOW_H
--- src/F_freepoll_window.cpp
+++ src/F_freepoll_window.cpp
@@ -1,6 +1,8 @@
 #include <FL/fl_ask.H>
 #include <FL/Fl_Pack.H>
 #include <FL/Fl_Pixmap.H>
+#include <iostream>
+#include <cassert>
 #include "datatypes.h"
 #include "course.h"
 #include "bar_graph_icon.h"
@@ -18,15 +20,15 @@ F_FreePollWindow::F_FreePollWindow( Base *base, DataStore *datastore )
   , m_datastore( datastore )
   , m_model( nullptr )
   , m_graph_shown( false )
-  , m_tile( 0, 0, WIDTH, HEIGHT )
-  , m_course_chooser( 0, COURSE_CHOOSER_Y, WIDTH, COURSE_CHOOSER_HEIGHT )
-  , m_controls( 0, CONTROLS_Y, WIDTH, CONTROLS_HEIGHT )
-  , m_poll_btn( 0, 0, POLL_BTN_WIDTH, POLL_BTN_HEIGHT )
-  , m_timer_display( 0, 0, TIMER_DISPLAY_WIDTH, TIMER_DISPLAY_HEIGHT, " 0:00" )
-  , m_count_display( 0, 0, COUNT_DISPLAY_WIDTH, COUNT_DISPLAY_HEIGHT, "  0" )
-  , m_gap( 0, 0, GAP_WIDTH, CONTROLS_HEIGHT )
-  , m_graph_btn( 0, 0, GRAPH_BTN_WIDTH, GRAPH_BTN_HEIGHT )
-  , m_graph_box( 0, BARGRAPH_Y, WIDTH, BARGRAPH_HEIGHT ) {
+  , m_pack( 0, 0, WIDTH, HEIGHT )
+  , m_course_chooser( 0, 0, WIDTH, COURSE_CHOOSER_HEIGHT )
+  , m_controls( 0, 0, WIDTH, CONTROLS_HEIGHT )
+  , m_poll_btn( POLL_BTN_X, CONTROL_Y, POLL_BTN_WIDTH, POLL_BTN_HEIGHT )
+  , m_timer_display( TIMER_DISPLAY_X, CONTROL_Y, TIMER_DISPLAY_WIDTH, TIMER_DISPLAY_HEIGHT, "0:00" )
+  , m_count_display( COUNT_DISPLAY_X, CONTROL_Y, COUNT_DISPLAY_WIDTH, COUNT_DISPLAY_HEIGHT, "0" )
+  , m_freq_display( FREQ_DISPLAY_X, CONTROL_Y, FREQ_DISPLAY_WIDTH, FREQ_DISPLAY_HEIGHT, "" )
+  , m_graph_btn( GRAPH_BTN_X, CONTROL_Y, GRAPH_BTN_WIDTH, GRAPH_BTN_HEIGHT )
+  , m_graph_box( 0, 0, WIDTH, BARGRAPH_HEIGHT ) {
 
   // Layout is
   //   +--------------------------------------------+
@@ -37,28 +39,39 @@ F_FreePollWindow::F_FreePollWindow( Base *base, DataStore *datastore )
   //   |                bar graph                   |
   //   +--------------------------------------------+
 
+  m_pack.type( Fl_Pack::VERTICAL );
+  m_pack.spacing( SPACING );
+
   m_graph_box.color( 0x0000FF00 );
   m_graph_box.box( FL_FLAT_BOX );
-
-  m_controls.type( Fl_Pack::HORIZONTAL );
-  m_controls.spacing( SPACING );
 
   m_poll_btn.label( "@>" );
   m_poll_btn.clear_visible_focus();
 
-  m_timer_display.labelsize( TIMER_DISPLAY_HEIGHT - 12 );
+  m_timer_display.labelsize( TEXT_SIZE );
   m_timer_display.labelcolor( DISABLED_TEXT_COLOR );
 
-  m_count_display.labelsize( COUNT_DISPLAY_HEIGHT - 12 );
+  m_count_display.labelsize( TEXT_SIZE );
   m_count_display.labelcolor( DISABLED_TEXT_COLOR );
+  //m_count_display.align( FL_ALIGN_RIGHT );
+
+  m_freq_display.labelsize( TEXT_SIZE );
 
   m_graph_btn.image( &bar_graph_pixmap );
   m_graph_btn.clear_visible_focus();
-  m_graph_btn.callback( on_graph_button_clicked, static_cast<void*>(this) );
 
+  // Populate courses in course chooser
   for ( auto course : m_datastore->get_courses() )
     m_course_chooser.add( course->get_display_string().c_str() );
   m_course_chooser.value( 0 );
+
+  // Display current frequency
+  update_frequency_display();
+
+  // register callbacks to handle UI events
+  m_graph_btn.callback( on_graph_button_clicked, static_cast<void*>(this) );
+  m_course_chooser.callback( on_course_change, static_cast<void*>( this ) );
+
 }
 
 F_FreePollWindow::~F_FreePollWindow() {
@@ -75,6 +88,11 @@ void F_FreePollWindow::on_update(Observable *observable, int hint) {
 
 }
 
+void F_FreePollWindow::on_course_change( Fl_Widget *w, void *data ) {
+  F_FreePollWindow *win = static_cast<F_FreePollWindow*>( data );
+  win->update_frequency_display();
+}
+
 void F_FreePollWindow::on_graph_button_clicked( Fl_Widget *w, void *data ) {
   F_FreePollWindow *win = static_cast<F_FreePollWindow*>( data );
   if ( win->m_graph_shown ) {
@@ -86,4 +104,14 @@ void F_FreePollWindow::on_graph_button_clicked( Fl_Widget *w, void *data ) {
     win->resize( win->x(), win->y(), win->w(), HEIGHT );
     win->m_graph_shown = true;
   }
+}
+
+void F_FreePollWindow::update_frequency_display() {
+  int course_index = m_course_chooser.value();
+  const auto &courses = m_datastore->get_courses();
+  Course *course = courses.at( course_index );
+  Frequency freq = course->get_frequency();
+  std::string freq_str = freq.str();
+  //std::cout << "freq_str=" << freq_str << "\n";
+  m_freq_display.copy_label( freq.str().c_str() );
 }
