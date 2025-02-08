6d9b1922e124a83fb3901e267db7aaf5e7b6afd5
David Hovemeyer <david.hovemeyer@gmail.com>
2025-02-08 08:13:28 -0500

    working on UI

--- include/F_freepoll_window.h
+++ include/F_freepoll_window.h
@@ -3,6 +3,9 @@
 
 #include <FL/Fl.H>
 #include <FL/Fl_Window.H>
+#include <FL/Fl_Tile.H>
+#include <FL/Fl_Box.H>
+#include <FL/Fl_Choice.H>
 #include "observer.h"
 #include "base.h"
 #include "datastore.h"
@@ -15,12 +18,27 @@
 class F_FreePollWindow : public Fl_Window, public Observer {
 public:
   static const int DEFAULT_WIDTH = 360;
-  static const int DEFAULT_HEIGHT = 100;
+
+  static const int DEFAULT_COURSE_CHOOSER_HEIGHT = 32;
+  static const int DEFAULT_CONTROLS_HEIGHT = 64;
+  static const int DEFAULT_BARGRAPH_HEIGHT = 200;
+
+  static const int DEFAULT_HEIGHT =
+    DEFAULT_COURSE_CHOOSER_HEIGHT +
+    DEFAULT_CONTROLS_HEIGHT +
+    DEFAULT_BARGRAPH_HEIGHT;
 
 private:
   Base *m_base;
   DataStore *m_datastore;
   PollModel *m_model;
+
+  // containers
+  Fl_Tile m_tile;
+  Fl_Choice m_course_chooser;
+  Fl_Box m_controls_box;
+  Fl_Box m_graph_box;
+  Fl_End m_tile_end;
 
 public:
   F_FreePollWindow( Base *base, DataStore *datastore );
--- src/F_freepoll_window.cpp
+++ src/F_freepoll_window.cpp
@@ -1,11 +1,39 @@
 #include <FL/fl_ask.H>
+#include <FL/Fl_Pack.H>
+#include "course.h"
 #include "F_freepoll_window.h"
 
 F_FreePollWindow::F_FreePollWindow( Base *base, DataStore *datastore )
   : Fl_Window( DEFAULT_WIDTH, DEFAULT_HEIGHT )
   , m_base( base )
   , m_datastore( datastore )
-  , m_model( nullptr ) {
+  , m_model( nullptr )
+  , m_tile( 0, 0, DEFAULT_WIDTH, DEFAULT_HEIGHT )
+  , m_course_chooser( 0, 0, DEFAULT_WIDTH, DEFAULT_COURSE_CHOOSER_HEIGHT )
+  , m_controls_box( 0, DEFAULT_COURSE_CHOOSER_HEIGHT, DEFAULT_WIDTH, DEFAULT_CONTROLS_HEIGHT )
+  , m_graph_box( 0, DEFAULT_COURSE_CHOOSER_HEIGHT+DEFAULT_CONTROLS_HEIGHT, DEFAULT_WIDTH, DEFAULT_BARGRAPH_HEIGHT ) {
+
+  // Layout is
+  //   +--------------------------------------------+
+  //   |              course chooser                |
+  //   +--------------------------------------------+
+  //   | [PollBtn] [Time] [Count] [Freq] [GraphBtn] |
+  //   +--------------------------------------------+
+  //   |                bar graph                   |
+  //   +--------------------------------------------+
+  //
+  // There are three Fl_Pack containers with horizontal
+  // layout for each of the three areas. A top-level Fl_Pack
+  // with vertical layout contains these Fl_Packs.
+
+  m_controls_box.color( 0xFF000000 );
+  m_controls_box.box( FL_FLAT_BOX );
+  m_graph_box.color( 0x0000FF00 );
+  m_graph_box.box( FL_FLAT_BOX );
+
+  for ( auto course : m_datastore->get_courses() )
+    m_course_chooser.add( course->get_display_string().c_str() );
+  m_course_chooser.value( 0 );
 }
 
 F_FreePollWindow::~F_FreePollWindow() {
