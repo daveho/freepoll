92714b59a9d462ed2b746e915074886f1cd59b3d
David Hovemeyer <david.hovemeyer@gmail.com>
2025-02-09 15:38:12 -0500

    move positions/sizes to .cpp file

--- include/F_freepoll_window.h
+++ include/F_freepoll_window.h
@@ -21,46 +21,6 @@
 // all model objects.
 class F_FreePollWindow : public Fl_Window, public Observer {
 public:
-  // for simplicity, all sizes and positions are hard-coded
-
-  static const int WIDTH = 360;
-
-  static const int COURSE_CHOOSER_Y = 0;
-  static const int COURSE_CHOOSER_HEIGHT = 24;
-  static const int CONTROLS_Y = COURSE_CHOOSER_Y + COURSE_CHOOSER_HEIGHT;
-  static const int CONTROLS_HEIGHT = 54;
-  static const int BARGRAPH_Y = CONTROLS_Y + CONTROLS_HEIGHT;
-  static const int BARGRAPH_HEIGHT = 140;
-
-  static const int HEIGHT = COURSE_CHOOSER_HEIGHT + CONTROLS_HEIGHT + BARGRAPH_HEIGHT;
-  static const int HEIGHT_NOGRAPH = COURSE_CHOOSER_HEIGHT + CONTROLS_HEIGHT;
-
-  static const int CONTROL_Y = 0;
-
-  static const int POLL_BTN_X = 0;
-  static const int POLL_BTN_WIDTH = CONTROLS_HEIGHT;
-  static const int POLL_BTN_HEIGHT = POLL_BTN_WIDTH;
-
-  static const int TIMER_DISPLAY_X = POLL_BTN_X + POLL_BTN_WIDTH;
-  static const int TIMER_DISPLAY_WIDTH = 96;
-  static const int TIMER_DISPLAY_HEIGHT = POLL_BTN_HEIGHT;
-
-  static const int COUNT_DISPLAY_X = TIMER_DISPLAY_X + TIMER_DISPLAY_WIDTH;
-  static const int COUNT_DISPLAY_WIDTH = 80;
-  static const int COUNT_DISPLAY_HEIGHT = POLL_BTN_HEIGHT;
-
-  static const int FREQ_DISPLAY_X = COUNT_DISPLAY_X + COUNT_DISPLAY_WIDTH;
-  static const int FREQ_DISPLAY_WIDTH = 64;
-  static const int FREQ_DISPLAY_HEIGHT = POLL_BTN_HEIGHT;
-
-  static const int TEXT_SIZE = POLL_BTN_HEIGHT - 16;
-
-  static const int GRAPH_BTN_WIDTH = POLL_BTN_WIDTH;
-  static const int GRAPH_BTN_X = WIDTH - GRAPH_BTN_WIDTH;
-  static const int GRAPH_BTN_HEIGHT = POLL_BTN_WIDTH;
-
-  static const Fl_Color DISABLED_TEXT_COLOR = 0x80808000;
-
 private:
   PollModel *m_model;
   DataStore *m_datastore;
